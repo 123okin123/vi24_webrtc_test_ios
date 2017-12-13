cd3c73336c06a3e0b738269cfd300eec301e5587
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-12-13 16:35:43 +0800

    Initial Commit

---
+++ VI24WebRTCTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VI24WebRTCTest
+//
+//  Created by Nikolai Kratz on 13/12/17.
+//  Copyright © 2017 Nikolai Kratz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
