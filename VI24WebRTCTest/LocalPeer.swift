899e01b15478c634a07cdd2c1cf4fd200cfa15e4
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-12-21 18:31:08 +0800

    ws

---
+++ VI24WebRTCTest/LocalPeer.swift
@@ -0,0 +1,14 @@
+//
+//  LocalPeer.swift
+//  VI24WebRTCTest
+//
+//  Created by Nikolai Kratz on 21/12/17.
+//  Copyright © 2017 Nikolai Kratz. All rights reserved.
+//
+
+import Foundation
+import WebRTC
+
+class LocalRTC {
+    
+}
---
+++ VI24WebRTCTest/WebSocket.swift
@@ -0,0 +1,9 @@
+//
+//  WebSocket.swift
+//  VI24WebRTCTest
+//
+//  Created by Nikolai Kratz on 21/12/17.
+//  Copyright © 2017 Nikolai Kratz. All rights reserved.
+//
+
+import Foundation
